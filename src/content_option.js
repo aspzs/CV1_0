0c104b2d9cc7c452e47be7a5383124026e992965
aspzs <25sparnol@gmail.com>
2023-04-26 16:59:06 -0600

    commit con imgs actualizadas

--- src/content_option.js
+++ src/content_option.js
@@ -12,7 +12,7 @@ const introdata = {
         third: "Hallo",
     },
     description: "Soy un artista multidisciplinario en constante crecimiento que utiliza diversos medios. Tengo habilidades en arte visual, frontend y un poco de backend. Me estoy educando en ámbitos filosóficos, tecnológicos y, por supuesto, artísticos.",
-    your_img_url: "https://raw.githubusercontent.com/aspzs/CV1.0/main/img/Fotos%20al%20azar/CNV000034.jpg",
+    your_img_url: "https://raw.githubusercontent.com/aspzs/IMGSCV1.0/main/CNV000034.jpg",
 };
 
 const dataabout = {
@@ -100,30 +100,30 @@ const services = [{
 ];
 
 const dataportfolio = [{
-        img: "https://raw.githubusercontent.com/aspzs/CV1.0/main/img/Made%20with%20processing/Aufmerksamkeit.gif",
+        img: "https://raw.githubusercontent.com/aspzs/IMGSCV1.0/main/Aufmerksamkeit.gif",
         description: "Made with processing 1",
         link: "https://aspzs.github.io/Made-with-processing/",
     },    
     {
-        img: "https://raw.githubusercontent.com/aspzs/CV1.0/main/img/Made%20with%20processing/webcam%20y%20glifos.png",
+        img: "https://raw.githubusercontent.com/aspzs/IMGSCV1.0/main/webcam%20y%20glifos.png",
         description: "Made with processing 2 - using webcam",
         link: "https://editor.p5js.org/aspzs/full/9tmlFlkWM",
     },
     {
-        img: "https://raw.githubusercontent.com/aspzs/CV1.0/main/img/Fotos%20al%20azar/toca-una-tecla.png",
+        img: "https://raw.githubusercontent.com/aspzs/IMGSCV1.0/main/toca-una-tecla.png",
         description: "Toca una tecla",
         link: "https://editor.p5js.org/aspzs/full/CyjWwZW-H",
     },
     {
-        img: "https://raw.githubusercontent.com/aspzs/CV1.0/main/img/Fotos%20al%20azar/zonamaco1.png",
+        img: "https://raw.githubusercontent.com/aspzs/IMGSCV1.0/main/zonamaco1.png",
         description: "Contenido Visual",
         link: "https://aspzs.github.io/Visual-Content/",
     },{
-        img: "https://raw.githubusercontent.com/aspzs/CV1.0/main/img/Fotos%20al%20azar/follow-mouse.png",
+        img: "https://raw.githubusercontent.com/aspzs/IMGSCV1.0/main/follow-mouse.png",
         description: "Siguiendo el mouse",
         link: "https://editor.p5js.org/aspzs/full/oEwWWLxGG",
     },{
-        img: "https://raw.githubusercontent.com/aspzs/CV1.0/main/img/Fotos%20al%20azar/tormento%20de%20cuahutemoc%2C%20segmentada2.png",
+        img: "https://raw.githubusercontent.com/aspzs/IMGSCV1.0/main/tormento%20de%20cuahutemoc%2C%20segmentada2.png",
         description: "Segmentación en Siqueiros",
         link: "https://aspzs.github.io/segmentacion-de-imagenes-en-siqueiros/",
     },
